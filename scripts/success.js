0caccfe1cf67629f6ae2d1a618494a03161c6ed5
nerdynischal <nerdynischal@gmail.com>
2025-06-17 19:44:00 +0100

    Update link

--- scripts/success.js
+++ scripts/success.js
@@ -3,8 +3,7 @@ const emailAddress = document.querySelector(".email-address");
 emailAddress.textContent = localStorage.getItem("myEmail");
 
 dismissButton.addEventListener("click", () => {
-  window.location.href =
-    "newsletter-sign-up-with-success-message-main/index.html";
+  window.location.href = "./index.html";
 });
 
 localStorage.clear(); //clear any leftover emails
